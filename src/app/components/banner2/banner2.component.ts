2b9030813583abf6aab42bbd1846b414a5ad4876
rodrigosanchonet <rodrigosancho.net@outlook.es>
2023-09-07 12:12:31 -0600

    Varios cambios se creo banner 2 y se mejoro el responsive

--- src/app/components/banner2/banner2.component.ts
+++ src/app/components/banner2/banner2.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import * as Aos from 'aos';
 @Component({
   selector: 'app-banner2',
   templateUrl: './banner2.component.html',
   styleUrls: ['./banner2.component.css']
 })
 export class Banner2Component {
-
+  ngOnInit() {
+    Aos.init();
+  }
 }
